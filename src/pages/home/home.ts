1c27ef233bdb56bc038356a8a31ebb1f81fed727
Mehdi Benadel <mehdi.benadel@gmail.com>
2017-01-04 18:08:36 +0100

    So style, Much pages, Wow

---
+++ src/pages/home/home.ts
@@ -0,0 +1,52 @@
+import { Component, ViewChild } from '@angular/core';
+
+import { Slides } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+  outputs: [ "closeEvent: close" ],
+  host: {
+      "(document:keydown)": "onKey($event)"
+  }
+})
+export class HomePage {
+  @ViewChild('mySlider') slider: Slides;
+
+  mySlideOptions = {
+    initialSlide: 0
+  };  
+  pageNumber = 1;
+  maxPages;
+   
+  onSlideChanged() {
+    this.pageNumber = this.slider.getActiveIndex() + 1;
+    this.maxPages = this.slider.length();
+  }
+   
+  onPageNumberChanged() {
+    this.slider.slideTo(this.pageNumber - 1);
+    this.maxPages = this.slider.length();
+  }
+
+  onKey(event: KeyboardEvent) {
+    
+    switch (event.key) {
+      case "ArrowUp":
+      case "ArrowLeft":
+        this.slider.slidePrev();
+        break;
+      case "ArrowDown":
+      case "ArrowRight":
+        this.slider.slideNext();
+        break;
+      default:
+    }
+    
+    this.onSlideChanged();
+    
+    return;
+
+  }
+
+}
